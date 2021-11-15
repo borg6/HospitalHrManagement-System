0c21472b1a17982d639327cb1638151191301b8d
borg6 <68291680+borg6@users.noreply.github.com>
2021-11-15 14:16:08 +0000

    netfilter: Change %p to %pK in debug messages

---
+++ examples/custom-server-express/server.js
@@ -0,0 +1,38 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const { bottender } = require('bottender');
+
+const app = bottender({
+  dev: process.env.NODE_ENV !== 'production',
+});
+
+const port = Number(process.env.PORT) || 5000;
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // If your express server is behind a proxy, you need to call `enable('trust proxy')`
+  // See: http://expressjs.com/en/guide/behind-proxies.html#express-behind-proxies
+  // server.enable('trust proxy');
+
+  const verify = (req, _, buf) => {
+    req.rawBody = buf.toString();
+  };
+  server.use(bodyParser.json({ verify }));
+  server.use(bodyParser.urlencoded({ extended: false, verify }));
+
+  server.get('/api', (req, res) => {
+    res.json({ ok: true });
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});

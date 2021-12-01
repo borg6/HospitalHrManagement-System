502e2d13eeb029749dbcb2111e8842400025a188
borg6 <68291680+borg6@users.noreply.github.com>
2021-12-01 10:46:20 +0000

    kj

---
+++ packages/bottender/src/__tests__/index.spec.ts
@@ -0,0 +1,60 @@
+import * as core from '..';
+
+describe('core', () => {
+  it('export bots', () => {
+    expect(core.Bot).toBeDefined();
+    expect(core.ConsoleBot).toBeDefined();
+    expect(core.MessengerBot).toBeDefined();
+    expect(core.WhatsappBot).toBeDefined();
+    expect(core.LineBot).toBeDefined();
+    expect(core.SlackBot).toBeDefined();
+    expect(core.TelegramBot).toBeDefined();
+    expect(core.ViberBot).toBeDefined();
+  });
+
+  it('export connectors', () => {
+    expect(core.ConsoleConnector).toBeDefined();
+    expect(core.FacebookBaseConnector).toBeDefined();
+    expect(core.MessengerConnector).toBeDefined();
+    expect(core.WhatsappConnector).toBeDefined();
+    expect(core.LineConnector).toBeDefined();
+    expect(core.SlackConnector).toBeDefined();
+    expect(core.TelegramConnector).toBeDefined();
+    expect(core.ViberConnector).toBeDefined();
+  });
+
+  it('export clients', () => {
+    expect(core.MessengerClient).toBeDefined();
+    expect(core.TwilioClient).toBeDefined();
+    expect(core.LineClient).toBeDefined();
+    expect(core.SlackOAuthClient).toBeDefined();
+    expect(core.TelegramClient).toBeDefined();
+    expect(core.ViberClient).toBeDefined();
+  });
+
+  it('export cache implements', () => {
+    expect(core.MemoryCacheStore).toBeDefined();
+    expect(core.RedisCacheStore).toBeDefined();
+  });
+
+  it('export session stores', () => {
+    expect(core.CacheBasedSessionStore).toBeDefined();
+    expect(core.MemorySessionStore).toBeDefined();
+    expect(core.RedisSessionStore).toBeDefined();
+    expect(core.FileSessionStore).toBeDefined();
+    expect(core.MongoSessionStore).toBeDefined();
+  });
+
+  it('export contexts', () => {
+    expect(core.Context).toBeDefined();
+    expect(core.ConsoleContext).toBeDefined();
+    expect(core.MessengerContext).toBeDefined();
+    expect(core.WhatsappContext).toBeDefined();
+    expect(core.LineContext).toBeDefined();
+    expect(core.SlackContext).toBeDefined();
+    expect(core.TelegramContext).toBeDefined();
+    expect(core.ViberContext).toBeDefined();
+  });
+
+  it('export extensions', () => {
+    expect(co

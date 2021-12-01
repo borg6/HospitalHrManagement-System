f07fc5036caf829df96c0258d49899220558a678
borg6 <68291680+borg6@users.noreply.github.com>
2021-12-01 14:21:04 +0000

    Auto update latest refs.

---
+++ packages/bottender/src/cli/providers/index.ts
@@ -0,0 +1,10 @@
+import line from './line';
+import messenger from './messenger';
+import sh from './sh';
+import telegram from './telegram';
+import viber from './viber';
+
+export default {
+  sh,
+  messenger,
+  teleg

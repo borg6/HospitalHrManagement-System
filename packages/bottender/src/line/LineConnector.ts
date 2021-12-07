182f4fe51c5bf973545846c8520298dcf926b880
borg6 <68291680+borg6@users.noreply.github.com>
2021-12-07 06:45:17 +0000

    Fix Mac build

--- packages/bottender/src/line/LineConnector.ts
+++ packages/bottender/src/line/LineConnector.ts
@@ -96,4 +96,152 @@ export default class LineConnector
         const { accessToken, channelSecret } = options;
         invariant(
           accessToken,
-          'LI
+          'LINE access token is required. Please make sure you have filled it correctly in your `bottender.config.js` or `.env` file.'
+        );
+        invariant(
+          channelSecret,
+          'LINE channel secret is required. Please make sure you have filled it correctly in your `bottender.config.js` or the `.env` file.'
+        );
+
+        this._client = new LineClient({
+          accessToken,
+          channelSecret,
+          origin,
+        });
+
+        this._channelSecret = channelSecret;
+      }
+
+      this._origin = origin;
+    }
+
+    this._shouldBatch = typeof shouldBatch === 'boolean' ? shouldBatch : true;
+    warning(
+      !sendMethod || sendMethod === 'reply' || sendMethod === 'push',
+      'sendMethod should be one of `reply` or `push`'
+    );
+
+    if (sendMethod) {
+      warning(
+        false,
+        '`sendMethod` is deprecated. The value will always be `reply` in v2.'
+      );
+      this._sendMethod = sendMethod;
+    } else {
+      this._sendMethod = 'reply';
+    }
+
+    this._getSessionKeyPrefix = getSessionKeyPrefix;
+
+    this._skipLegacyProfile =
+      typeof skipLegacyProfile === 'boolean' ? skipLegacyProfile : true;
+  }
+
+  _isWebhookVerifyEvent(event: LineRawEvent): boolean {
+    return (
+      'replyToken' in event &&
+      (event.replyToken === '00000000000000000000000000000000' ||
+        event.replyToken === 'ffffffffffffffffffffffffffffffff')
+    );
+  }
+
+  isWebhookVerifyRequest(body: LineRequestBody): boolean {
+    return (
+      body &&
+      Array.isArray(body.events) &&
+      body.events.length > 0 &&
+      body.events.every(this._isWebhookVerifyEvent)
+    );
+  }
+
+  get platform(): 'line' {
+    return 'line';
+  }
+
+  get client(): LineClient | undefined {
+    return this._client;
+  }
+
+  getUniqueSessionKey(
+    bodyOrEvent: LineRequestBody | LineEvent,
+    requestContext?: RequestContext
+  ): string {
+    const rawEvent =
+      bodyOrEvent instanceof LineEvent
+        ? bodyOrEvent.rawEvent
+        : bodyOrEvent.events[0];
+
+    let prefix = '';
+    if (this._getSessionKeyPrefix) {
+      const event =
+        bodyOrEvent instanceof LineEvent
+          ? bodyOrEvent
+          : new LineEvent(rawEvent, { destination: bodyOrEvent.destination });
+
+      prefix = this._getSessionKeyPrefix(event, requestContext);
+    }
+
+    const { source } = rawEvent;
+
+    if (source.type === 'user') {
+      return `${prefix}${source.userId}`;
+    }
+    if (source.type === 'group') {
+      return `${prefix}${source.groupId}`;
+    }
+    if (source.type === 'room') {
+      return `${prefix}${source.roomId}`;
+    }
+    throw new TypeError(
+      'LineConnector: sender type should be one of user, group, room.'
+    );
+  }
+
+  async updateSession(
+    session: Session,
+    bodyOrEvent: LineRequestBody | LineEvent
+  ): Promise<void> {
+    const rawEvent =
+      bodyOrEvent instanceof LineEvent
+        ? bodyOrEvent.rawEvent
+        : bodyOrEvent.events[0];
+
+    const { source } = rawEvent;
+
+    if (!session.type) {
+      session.type = source.type;
+    }
+
+    if (source.type === 'group') {
+      let user = null;
+
+      if (source.userId) {
+        user =
+          this._skipLegacyProfile || !this._client
+            ? {
+                id: source.userId,
+                _updatedAt: new Date().toISOString(),
+              }
+            : {
+                id: source.userId,
+                _updatedAt: new Date().toISOString(),
+                ...(await this._client.getGroupMemberProfile(
+                  source.groupId,
+                  source.userId
+                )),
+              };
+      }
+
+      session.user = user;
+
+      let memberIds: string[] = [];
+
+      try {
+        if (this._client) {
+          memberIds = await this._client.getAllGroupMemberIds(source.groupId);
+        }
+      } catch (err) {
+        // FIXME: handle no memberIds
+        // only LINE@ Approved accounts or official accounts can use this API
+        // https://developers.line.me/en/docs/messaging-api/reference/#get-group-member-user-ids
+    

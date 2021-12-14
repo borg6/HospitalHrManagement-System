d1f950e1bce03d8e5b625fc0339cb9ff741414b1
borg6 <68291680+borg6@users.noreply.github.com>
2021-12-14 13:51:29 +0000

    Pi-Ager_i2c_off.conf.on

--- packages/bottender/src/line/__tests__/LineConnector.spec.ts
+++ packages/bottender/src/line/__tests__/LineConnector.spec.ts
@@ -174,3 +174,154 @@ describe('#getUniqueSessionKey', () => {
 
   it('extract groupId from the group source', () => {
     const { connector } = setup();
+
+    const senderId = connector.getUniqueSessionKey(
+      new LineEvent({
+        replyToken: 'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
+        type: 'message',
+        mode: 'active',
+        timestamp: 1462629479859,
+        source: {
+          type: 'group',
+          groupId: 'U206d25c2ea6bd87c17655609a1c37cb8',
+        },
+        message: {
+          id: '325708',
+          type: 'text',
+          text: 'Hello, world',
+        },
+      })
+    );
+
+    expect(senderId).toBe('U206d25c2ea6bd87c17655609a1c37cb8');
+  });
+
+  it('extract roomId from the room source', () => {
+    const { connector } = setup();
+
+    const senderId = connector.getUniqueSessionKey(
+      new LineEvent({
+        replyToken: 'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
+        type: 'message',
+        mode: 'active',
+        timestamp: 1462629479859,
+        source: {
+          type: 'room',
+          roomId: 'U206d25c2ea6bd87c17655609a1c37cb8',
+        },
+        message: {
+          id: '325708',
+          type: 'text',
+          text: 'Hello, world',
+        },
+      })
+    );
+
+    expect(senderId).toBe('U206d25c2ea6bd87c17655609a1c37cb8');
+  });
+
+  it('should add the prefix to the session key when getSessionKeyPrefix exists', () => {
+    const getSessionKeyPrefix: GetSessionKeyPrefixFunction = jest.fn(
+      (_, requestContext) => {
+        if (requestContext) {
+          return `${requestContext.params.channelId}:`;
+        }
+        throw new Error('no request context');
+      }
+    );
+    const { connector } = setup({
+      getSessionKeyPrefix,
+    });
+
+    const requestContext = {
+      method: 'post',
+      path: '/webhooks/line/CHANNEL_ID',
+      query: {},
+      headers: {},
+      rawBody: '{}',
+      body: {},
+      params: {
+        channelId: 'CHANNEL_ID',
+      },
+      url: 'https://www.example.com/webhooks/line/CHANNEL_ID',
+    };
+
+    const senderId = connector.getUniqueSessionKey(
+      new LineEvent({
+        replyToken: 'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
+        type: 'message',
+        mode: 'active',
+        timestamp: 1462629479859,
+        source: {
+          type: 'user',
+          userId: 'U206d25c2ea6bd87c17655609a1c37cb8',
+        },
+        message: {
+          id: '325708',
+          type: 'text',
+          text: 'Hello, world',
+        },
+      }),
+      requestContext
+    );
+
+    expect(senderId).toBe('CHANNEL_ID:U206d25c2ea6bd87c17655609a1c37cb8');
+    expect(getSessionKeyPrefix).toBeCalledWith(
+      expect.any(LineEvent),
+      requestContext
+    );
+  });
+});
+
+describe('#updateSession', () => {
+  it('update session with data needed', async () => {
+    const { connector, client } = setup({
+      skipLegacyProfile: false,
+    });
+
+    const user = {
+      id: 'U206d25c2ea6bd87c17655609a1c37cb8',
+      displayName: 'LINE taro',
+      userId: 'U206d25c2ea6bd87c17655609a1c37cb8',
+      pictureUrl: 'http://obs.line-apps.com/...',
+      statusMessage: 'Hello, LINE!',
+      _updatedAt: expect.any(String),
+    };
+    mocked(client).getUserProfile.mockResolvedValue(user);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const session: any = {};
+
+    await connector.updateSession(session, requestBody);
+
+    expect(client.getUserProfile).toBeCalledWith(
+      'U206d25c2ea6bd87c17655609a1c37cb8'
+    );
+
+    expect(session).toEqual({
+      type: 'user',
+      user,
+    });
+    expect(Object.isFrozen(session.user)).toBe(true);
+    expect(Object.getOwnPropertyDescriptor(session, 'user')).toEqual({
+      configurable: false,
+      enumerable: true,
+      writable: false,
+      value: session.user,
+    });
+  });
+
+  it('update session if session.user exists', async () => {
+    const { connector, client } = setup({
+      skipLegacyProfile: false,
+    });
+    const user = {
+      id: 'U206d25c2ea6bd87c17655609a1c37cb8',
+      displayName: 'LINE taro',
+      userId: 'U206d25c2ea6bd87c17655609a1c37cb8',
+      pictureUrl: 'http://obs.line-apps.com/...',
+      statusMessage: 'Hello, LINE!',
+      _updatedAt: expect.any(String),
+    };
+
+    const session = { type: 'user', use

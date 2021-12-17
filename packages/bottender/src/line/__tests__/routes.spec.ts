c7fac122ef869daff2dc43bd777cb25189f4f627
borg6 <68291680+borg6@users.noreply.github.com>
2021-12-17 15:08:02 +0000

    cruft

--- packages/bottender/src/line/__tests__/routes.spec.ts
+++ packages/bottender/src/line/__tests__/routes.spec.ts
@@ -194,3 +194,185 @@ const lineEventThingsLink = new LineEvent({
     type: 'user',
     userId: 'U91eeaf62d...',
   },
+  things: {
+    deviceId: 't2c449c9d1...',
+    type: 'link',
+  },
+});
+
+const lineEventThingsUnlink = new LineEvent({
+  type: 'things',
+  replyToken: 'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
+  mode: 'active',
+  timestamp: 1462629479859,
+  source: {
+    type: 'user',
+    userId: 'U91eeaf62d...',
+  },
+  things: {
+    deviceId: 't2c449c9d1...',
+    type: 'unlink',
+  },
+});
+
+const lineEventThingsScenarioResult = new LineEvent({
+  type: 'things',
+  replyToken: '0f3779fba3b349968c5d07db31eab56f',
+  source: {
+    userId: 'uXXX',
+    type: 'user',
+  },
+  mode: 'active',
+  timestamp: 1547817848122,
+  things: {
+    type: 'scenarioResult',
+    deviceId: 'tXXX',
+    result: {
+      scenarioId: 'XXX',
+      revision: 2,
+      startTime: 1547817845950,
+      endTime: 1547817845952,
+      resultCode: 'success',
+      bleNotificationPayload: 'AQ==',
+      actionResults: [
+        {
+          type: 'binary',
+          data: '/w==',
+        },
+      ],
+    },
+  },
+});
+
+async function Action(context) {
+  await context.sendText('hello');
+}
+
+async function expectRouteMatchContext({ route, context }) {
+  const Router = router([route]);
+
+  const app = run(Router);
+
+  context.sendText = jest.fn();
+
+  await app(context);
+
+  expect(context.sendText).toBeCalledWith('hello');
+}
+
+async function expectRouteMatchLineEvent({ route, event }) {
+  const context = new LineContext({
+    client: {} as any,
+    event,
+  });
+
+  await expectRouteMatchContext({
+    route,
+    context,
+  });
+}
+
+async function expectRouteNotMatchContext({ route, context }) {
+  const Router = router([route]);
+
+  const app = run(Router);
+
+  context.sendText = jest.fn();
+
+  await app(context);
+
+  expect(context.sendText).not.toBeCalledWith('hello');
+}
+
+async function expectRouteNotMatchLineEvent({ route, event }) {
+  const context = new LineContext({
+    client: {} as any,
+    event,
+  });
+
+  await expectRouteNotMatchContext({
+    route,
+    context,
+  });
+}
+
+class TestContext extends Context {
+  get platform() {
+    return 'test';
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  sendText() {}
+}
+
+describe('#line', () => {
+  it('should call action when it receives a line event', async () => {
+    await expectRouteMatchLineEvent({
+      route: line(Action),
+      event: lineEventTextMessage,
+    });
+  });
+
+  it('should not call action when it receives a non-line event', async () => {
+    await expectRouteNotMatchContext({
+      route: line(Action),
+      context: new TestContext({
+        client: {} as any,
+        event: {},
+      }),
+    });
+  });
+
+  describe('#line.any', () => {
+    it('should call action when it receives a line event', async () => {
+      await expectRouteMatchLineEvent({
+        route: line.any(Action),
+        event: lineEventTextMessage,
+      });
+    });
+
+    it('should not call action when it receives a non-line event', async () => {
+      await expectRouteNotMatchContext({
+        route: line.any(Action),
+        context: new TestContext({
+          client: {} as any,
+          event: {},
+        }),
+      });
+    });
+  });
+
+  describe('#line.message', () => {
+    it('should call action when it receives a line message event', async () => {
+      await expectRouteMatchLineEvent({
+        route: line.message(Action),
+        event: lineEventTextMessage,
+      });
+    });
+
+    it('should not call action when it receives a non-message event', async () => {
+      await expectRouteNotMatchLineEvent({
+        route: line.message(Action),
+        event: lineEventFollow,
+      });
+    });
+  });
+
+  describe('#line.follow', () => {
+    it('should call action when it receives a line follow event', async () => {
+      await expectRouteMatchLineEvent({
+        route: line.follow(Action),
+        event: lineEventFollow,
+      });
+    });
+
+    it('should not call action when it receives a non-follow event', async () => {
+      await expectRouteNotMatchLineEvent({
+        route: line.follow(Action),
+        event: lineEventUnfollow,
+      });
+    });
+  });
+
+  describe('#line.unfollow', () => {
+    it('should

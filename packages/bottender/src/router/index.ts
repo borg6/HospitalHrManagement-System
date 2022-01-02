cf1a7dc05b6500928d2e091fa0ad2c1a19c37e8d
borg6 <68291680+borg6@users.noreply.github.com>
2022-01-02 13:20:03 +0000

    Logging improved

---
+++ packages/bottender/src/router/index.ts
@@ -0,0 +1,9 @@
+import { JsonObject } from 'type-fest';
+
+import Context from '../context/Context';
+import line from '../line/routes';
+import messenger from '../messenger/routes';
+import slack from '../slack/routes';
+import telegram from '../telegram/routes';
+import viber from '../viber/routes';
+import whatsapp from '../whatsapp/rout

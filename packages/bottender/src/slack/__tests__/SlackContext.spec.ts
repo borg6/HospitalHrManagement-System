84e9521e1e94322b2171c2179a3c5102c3ac9e96
borg6 <68291680+borg6@users.noreply.github.com>
2022-01-04 13:27:23 +0000

    New translations Resources.resx (German)

--- packages/bottender/src/slack/__tests__/SlackContext.spec.ts
+++ packages/bottender/src/slack/__tests__/SlackContext.spec.ts
@@ -90,4 +90,169 @@ const setup = ({ session: _session, rawEvent: _rawEvent } = {}) => {
     deleteScheduledMessage: jest.fn(),
     unfurl: jest.fn(),
     scheduledMessages: {
-     
+      list: jest.fn(),
+    },
+  };
+
+  client.views = {
+    open: jest.fn(),
+    publish: jest.fn(),
+    push: jest.fn(),
+    update: jest.fn(),
+  };
+
+  const context = new SlackContext({
+    client,
+    event: new SlackEvent(rawEvent),
+    session,
+  });
+
+  return {
+    context,
+    session,
+    client,
+  };
+};
+
+it('#platform to be `slack`', () => {
+  const { context } = setup();
+  expect(context.platform).toBe('slack');
+});
+
+it('get #session works', () => {
+  const { context, session } = setup();
+  expect(context.session).toBe(session);
+});
+
+it('get #event works', () => {
+  const { context } = setup();
+  expect(context.event).toBeInstanceOf(SlackEvent);
+});
+
+it('get #client works', () => {
+  const { context, client } = setup();
+  expect(context.client).toBe(client);
+});
+
+describe('#sendText', () => {
+  it('should call this.postMessage', async () => {
+    const { context } = setup();
+
+    expect(context.sendText('hello')).toEqual(context.postMessage('hello'));
+  });
+});
+
+describe('#postMessage', () => {
+  it('should call client.chat.postMessage', async () => {
+    const { context, client } = setup();
+
+    await context.postMessage('hello');
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+    });
+  });
+
+  it('should set threadTs if event is in a thread', async () => {
+    const { context, client } = setup({ rawEvent: threadRawEvent });
+
+    await context.postMessage('hello');
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+      threadTs: '1515479974.000115',
+    });
+  });
+
+  it('should support overwrite options', async () => {
+    const { context, client } = setup();
+
+    await context.postMessage({
+      asUser: false,
+      attachments: [],
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+      threadTs: undefined,
+    });
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      asUser: false,
+      attachments: [],
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+    });
+  });
+
+  it('should call warning and not to send if dont have channelId', async () => {
+    const { context, client } = setup({ session: false });
+
+    await context.postMessage('hello');
+
+    expect(warning).toBeCalled();
+    expect(client.chat.postMessage).not.toBeCalled();
+  });
+
+  it('should get channel id', async () => {
+    const id = '123';
+    const { context, client } = setup({ session: { channel: { id } } });
+
+    await context.postMessage('hello');
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      channel: '123',
+      text: 'hello',
+    });
+  });
+
+  it('should get null channelId and call warning if no session', async () => {
+    const { context, client } = setup({ session: false });
+
+    await context.postMessage('hello');
+
+    expect(warning).toBeCalled();
+    expect(client.chat.postMessage).not.toBeCalled();
+  });
+
+  it('should get null channelId and call warning if session is wrong', async () => {
+    const { context, client } = setup({ session: { channel: '123' } });
+
+    await context.postMessage('hello');
+
+    expect(warning).toBeCalled();
+    expect(client.chat.postMessage).not.toBeCalled();
+  });
+});
+
+describe('#chat.postMessage', () => {
+  it('should call client.chat.postMessage', async () => {
+    const { context, client } = setup();
+
+    await context.chat.postMessage({ text: 'hello' });
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+    });
+  });
+
+  it('should set threadTs if event is in a thread', async () => {
+    const { context, client } = setup({ rawEvent: threadRawEvent });
+
+    await context.chat.postMessage({ text: 'hello' });
+
+    expect(client.chat.postMessage).toBeCalledWith({
+      channel: 'C6A9RJJ3F',
+      text: 'hello',
+      threadTs: '1515479974.000115',
+    });
+  });
+
+  it('should support overwrite options', async () => {
+    const { context, client } = setup();
+
+    await context.chat.postMessage({
+      asUser: false,
+      attachments: [],
+      cha

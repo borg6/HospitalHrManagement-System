f574d988b4c23ae8dfd635f4cb2a1e750ed8526a
borg6 <68291680+borg6@users.noreply.github.com>
2022-01-05 11:44:14 +0000

    Updating Docs

--- packages/bottender/src/telegram/TelegramConnector.ts
+++ packages/bottender/src/telegram/TelegramConnector.ts
@@ -99,4 +99,130 @@ export default class TelegramConnector
     if (body.shippingQuery) {
       return `${body.shippingQuery.from.id}`;
     }
-    if (body.preCheckoutQuery)
+    if (body.preCheckoutQuery) {
+      return `${body.preCheckoutQuery.from.id}`;
+    }
+    return '';
+  }
+
+  async updateSession(
+    session: Session,
+    body: TelegramRequestBody
+  ): Promise<void> {
+    let channel;
+    if (body.channelPost) {
+      channel = body.channelPost.chat;
+    } else if (body.editedChannelPost) {
+      channel = body.editedChannelPost.chat;
+    }
+
+    if (channel) {
+      if (this._skipLegacyProfile) {
+        session.channel = {
+          id: channel.id,
+        };
+      } else {
+        session.channel = channel;
+      }
+      session.channel._updatedAt = new Date().toISOString();
+      Object.freeze(session.channel);
+    }
+    Object.defineProperty(session, 'channel', {
+      configurable: false,
+      enumerable: true,
+      writable: false,
+      value: session.channel,
+    });
+
+    let group;
+    if (body.message && body.message.chat.type === 'group') {
+      group = body.message.chat;
+    } else if (body.editedMessage && body.editedMessage.chat.type === 'group') {
+      group = body.editedMessage.chat;
+    } else if (
+      body.callbackQuery &&
+      body.callbackQuery.message &&
+      body.callbackQuery.message.chat.type === 'group'
+    ) {
+      group = body.callbackQuery.message.chat;
+    }
+    if (group) {
+      if (this._skipLegacyProfile) {
+        session.group = {
+          id: group.id,
+        };
+      } else {
+        session.group = group;
+      }
+      session.group._updatedAt = new Date().toISOString();
+      Object.freeze(session.group);
+    }
+    Object.defineProperty(session, 'group', {
+      configurable: false,
+      enumerable: true,
+      writable: false,
+      value: session.group,
+    });
+
+    let user;
+    if (body.message) {
+      user = body.message.from;
+    } else if (body.editedMessage) {
+      user = body.editedMessage.from;
+    } else if (body.inlineQuery) {
+      user = body.inlineQuery.from;
+    } else if (body.chosenInlineResult) {
+      user = body.chosenInlineResult.from;
+    } else if (body.callbackQuery) {
+      user = body.callbackQuery.from;
+    } else if (body.shippingQuery) {
+      user = body.shippingQuery.from;
+    } else if (body.preCheckoutQuery) {
+      user = body.preCheckoutQuery.from;
+    }
+
+    if (user) {
+      if (this._skipLegacyProfile) {
+        session.user = {
+          id: user.id,
+        };
+      } else {
+        session.user = user;
+      }
+      session.user._updatedAt = new Date().toISOString();
+      Object.freeze(session.user);
+    }
+    Object.defineProperty(session, 'user', {
+      configurable: false,
+      enumerable: true,
+      writable: false,
+      value: session.user,
+    });
+  }
+
+  mapRequestToEvents(body: TelegramRequestBody): TelegramEvent[] {
+    const rawEvent = this._getRawEventFromRequest(body);
+    return [new TelegramEvent(rawEvent)];
+  }
+
+  createContext(params: {
+    event: TelegramEvent;
+    session: Session | null;
+    initialState?: JsonObject | null;
+    requestContext?: TelegramRequestContext;
+    emitter?: EventEmitter | null;
+  }): TelegramContext {
+    return new TelegramContext({
+      ...params,
+      client: this._client,
+    });
+  }
+
+  preprocess(): {
+    shouldNext: true;
+  } {
+    return {
+      shouldNext: true,
+    };
+  }
+}

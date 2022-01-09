a85178aa1ece65311e966da588cf339a9fd0f61c
borg6 <68291680+borg6@users.noreply.github.com>
2022-01-09 16:14:38 +0000

    fix scss

--- packages/bottender-facebook/src/FacebookClient.ts
+++ packages/bottender-facebook/src/FacebookClient.ts
@@ -127,4 +127,147 @@ export default class FacebookClient extends MessengerClient {
         ? {
             summary: {
               order: 'ranked' | 'chronological' | 'reverse_chronological';
-        
+              totalCount: number;
+              canComment: boolean;
+            };
+          }
+        : any)
+  > {
+    const conjunctFields = Array.isArray(fields) ? fields.join(',') : fields;
+
+    return this.axios
+      .get<
+        Types.PagingData<
+          Pick<
+            Types.Comment,
+            Types.CamelCaseUnion<Types.CommentKeyMap, typeof fields[number]>
+          >[]
+        > &
+          (U extends true
+            ? {
+                summary: {
+                  order: 'ranked' | 'chronological' | 'reverse_chronological';
+                  totalCount: number;
+                  canComment: boolean;
+                };
+              }
+            : any)
+      >(`/${objectId}/comments`, {
+        params: {
+          limit,
+          summary: summary ? 'true' : undefined,
+          filter,
+          order,
+          fields: conjunctFields,
+          access_token: this.accessToken,
+        },
+      })
+      .then((res) => res.data, handleError);
+  }
+
+  /**
+   * Get the data of likes on the object.
+   *
+   * @see https://developers.facebook.com/docs/graph-api/reference/object/likes
+   *
+   * @param objectId - ID of the comment.
+   * @param options -
+   */
+  public getLikes(
+    objectId: string,
+    { summary }: Types.GetLikesOptions = {}
+  ): Promise<Types.Likes> {
+    return this.axios
+      .get<{
+        id: string;
+        likes: Types.Likes;
+      }>(`/${objectId}/likes`, {
+        params: {
+          summary: summary ? 'true' : undefined,
+          access_token: this.accessToken,
+        },
+      })
+      .then((res) => res.data.likes, handleError);
+  }
+
+  /**
+   * Get metrics for pages.
+   *
+   * @see https://developers.facebook.com/docs/graph-api/reference/v7.0/insights
+   *
+   * @param options -
+   */
+  public getPageInsights<
+    D extends (
+      | ({
+          id: string;
+          title: string;
+          description: string;
+          period: P;
+        } & {
+          name: Exclude<
+            P extends 'day'
+              ? Types.PageInsightsMetricDay
+              : P extends 'week'
+              ? Types.PageInsightsMetricWeek
+              : P extends 'days_28'
+              ? Types.PageInsightsMetricDays28
+              : never,
+            | 'page_tab_views_login_top_unique'
+            | 'page_tab_views_login_top'
+            | 'page_tab_views_logout_top'
+            | 'page_negative_feedback_by_type'
+            | 'page_positive_feedback_by_type'
+            | 'page_fans_by_like_source'
+            | 'page_fans_by_like_source_unique'
+            | 'page_fans_by_unlike_source_unique'
+            | 'page_fans_by_unlike_source'
+            | 'page_content_activity_by_action_type_unique'
+            | 'page_content_activity_by_action_type'
+          >;
+          values: {
+            value: number;
+            endTime: string;
+          }[];
+        })
+      | {
+          name:
+            | 'page_tab_views_login_top_unique'
+            | 'page_tab_views_login_top'
+            | 'page_tab_views_logout_top';
+          values: {
+            value: {
+              allactivity?: number;
+              app?: number;
+              info?: number;
+              insights?: number;
+              likes?: number;
+              locations?: number;
+              photos?: number;
+              photosAlbums?: number;
+              photosStream?: number;
+              profile?: number;
+              profileInfo?: number;
+              profileLikes?: number;
+              profilePhotos?: number;
+              timeline?: number;
+              events?: number;
+              videos?: number;
+              wall?: number;
+              tabHome?: number;
+              reviews?: number;
+              about?: number;
+              profileHome?: number;
+              profileVideos?: number;
+              profilePosts?: number;
+              community?: number;
+              posts?: number;
+              home?: number;
+            };
+            endTime: string;
+          }[];
+        }
+      | {
+          name: 'page_negative_feedback_by_type';
+          values: {
+            valu

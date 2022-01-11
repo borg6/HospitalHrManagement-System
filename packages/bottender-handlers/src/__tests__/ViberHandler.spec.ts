e6027c3eef0498ff6e6f38e8cd10069f2cfc877e
borg6 <68291680+borg6@users.noreply.github.com>
2022-01-11 13:29:39 +0000

    bumping version

--- packages/bottender-handlers/src/__tests__/ViberHandler.spec.ts
+++ packages/bottender-handlers/src/__tests__/ViberHandler.spec.ts
@@ -203,4 +203,163 @@ describe('#onConversationStarted', () => {
       event: {
         isConversationStarted: false,
         conversationStarted: null,
- 
+      },
+    };
+    builder.onConversationStarted(handler);
+    await builder.build()(context);
+    expect(handler).not.toBeCalled();
+  });
+});
+
+describe('#onDelivered', () => {
+  const delivered = {
+    event: 'delivered',
+    timestamp: 1457764197627,
+    message_token: 4912661846655238145,
+    user_id: '01234567890A=',
+  };
+
+  it('should return this', async () => {
+    const { builder } = setup();
+    const handler = () => {};
+    expect(await builder.onDelivered(handler)).toBe(builder);
+  });
+
+  it('should support catch all handler', async () => {
+    const { builder } = setup();
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isDelivered: true,
+        delivered,
+      },
+    };
+    builder.onDelivered(handler);
+    await builder.build()(context);
+    expect(handler).toBeCalledWith(context);
+  });
+
+  it('should call predicate when received onDelivered event', async () => {
+    const { builder } = setup();
+    const predicate = jest.fn(() => true);
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isDelivered: true,
+        delivered,
+      },
+    };
+    builder.onDelivered(predicate, handler);
+    await builder.build()(context);
+    expect(predicate).toBeCalledWith(delivered, context);
+    expect(handler).toBeCalledWith(context);
+  });
+
+  it('should not call handler when received not delivered event', async () => {
+    const { builder } = setup();
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isDelivered: false,
+        delivered: null,
+      },
+    };
+    builder.onDelivered(handler);
+    await builder.build()(context);
+    expect(handler).not.toBeCalled();
+  });
+});
+
+describe('#onSeen', () => {
+  const seen = {
+    event: 'seen',
+    timestamp: 1457764197627,
+    message_token: 4912661846655238145,
+    user_id: '01234567890A=',
+  };
+
+  it('should return this', async () => {
+    const { builder } = setup();
+    const handler = () => {};
+    expect(await builder.onSeen(handler)).toBe(builder);
+  });
+
+  it('should support catch all handler', async () => {
+    const { builder } = setup();
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isSeen: true,
+        seen,
+      },
+    };
+    builder.onSeen(handler);
+    await builder.build()(context);
+    expect(handler).toBeCalledWith(context);
+  });
+
+  it('should call predicate when received onSeen event', async () => {
+    const { builder } = setup();
+    const predicate = jest.fn(() => true);
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isSeen: true,
+        seen,
+      },
+    };
+    builder.onSeen(predicate, handler);
+    await builder.build()(context);
+    expect(predicate).toBeCalledWith(seen, context);
+    expect(handler).toBeCalledWith(context);
+  });
+
+  it('should not call handler when received not seen event', async () => {
+    const { builder } = setup();
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isSeen: false,
+        seen: null,
+      },
+    };
+    builder.onSeen(handler);
+    await builder.build()(context);
+    expect(handler).not.toBeCalled();
+  });
+});
+
+describe('#onFailed', () => {
+  const failed = {
+    event: 'failed',
+    timestamp: 1457764197627,
+    message_token: 4912661846655238145,
+    user_id: '01234567890A=',
+    desc: 'failure description',
+  };
+
+  it('should return this', async () => {
+    const { builder } = setup();
+    const handler = () => {};
+    expect(await builder.onFailed(handler)).toBe(builder);
+  });
+
+  it('should support catch all handler', async () => {
+    const { builder } = setup();
+    const handler = jest.fn();
+    const context = {
+      event: {
+        isFailed: true,
+        failed,
+      },
+    };
+    builder.onFailed(handler);
+    await builder.build()(context);
+    expect(handler).toBeCalledWith(context);
+  });
+
+  it('should call predicate when received onFailed event', async () => {
+    const { builder } = setup();
+    const predicate = jest.fn(() => true);
+    const handler = jest.fn();
+    const context = {

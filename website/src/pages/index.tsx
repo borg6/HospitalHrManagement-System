e37c73ce0ade8ccf789ca9e7f8303000ca7a4be8
borg6 <68291680+borg6@users.noreply.github.com>
2022-02-23 09:45:41 +0000

    added zip

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -92,4 +92,115 @@ const Index: FC = () => {
                         You can apply progressive enhancement or graceful
                         degradation strategy on your building blocks.
                       </p>
-               
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="wrapper gridBlock">
+              <div className="blockElement">
+                <div className="blockImage">
+                  <img src="img/element_modular.svg" alt="Easy Setup" />
+                </div>
+                <div className="blockContent">
+                  <h2>Easy Setup</h2>
+                  <p>
+                    With Bottender, you only need a few configurations to make
+                    your bot work with channels, automatic server listening,
+                    webhook setup, signature verification and so much more.
+                  </p>
+                </div>
+              </div>
+              <div className="blockElement">
+                <div className="blockImage">
+                  <img
+                    src="img/element_anywhere.svg"
+                    alt="Ready for Production"
+                  />
+                </div>
+                <div className="blockContent">
+                  <h2>Ready for Production</h2>
+                  <p>There are thousands of bots powered by Bottender.</p>
+                  <p>
+                    It has been optimized for real world use cases, automatic
+                    batching request and dozens of other compelling features.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="triangle" />
+
+        <div className="demo">
+          <div className="demo-content">
+            <div className="code-block">
+              {/* eslint-disable react/jsx-props-no-spreading,react/no-array-index-key */}
+              <Highlight
+                {...defaultProps}
+                code={sampleCode}
+                theme={theme}
+                language="javascript"
+              >
+                {({
+                  className,
+                  style,
+                  tokens,
+                  getLineProps,
+                  getTokenProps,
+                }) => (
+                  <pre
+                    className={className}
+                    style={{ ...style, padding: '20px' }}
+                  >
+                    {tokens.map((line, i) => (
+                      <div key={i} {...getLineProps({ line, key: i })}>
+                        {line.map((token, key) => (
+                          <span key={key} {...getTokenProps({ token, key })} />
+                        ))}
+                      </div>
+                    ))}
+                  </pre>
+                )}
+              </Highlight>
+              {/* eslint-enable react/jsx-props-no-spreading */}
+            </div>
+            <div className="description">
+              <h3>Intuitive APIs</h3>
+              <p>
+                Bottender has some functional and declarative approaches can
+                help you define your conversations.
+              </p>
+              <p>
+                For most applications, you will begin by defining routes that
+                you may familiar with when developing a web application.
+              </p>
+              <p>
+                Besides router, you can describe the side effects with function
+                actions. It’s the most important building part in the Bottender
+                applications.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="get-started">
+          <div className="get-started-card">
+            <div className="get-started-card-background">
+              <div className="primary-box" />
+              <div className="secondary-box" />
+            </div>
+            <img
+              src="img/element_build_your_bot_logo.svg"
+              alt="Bottender Logo"
+            />
+            <div className="get-started-content">
+              <h2>Build Your Bot Today!</h2>
+              <Link className="primary large" to="docs/">
+                Get Started Now
+              </Link>
+            </div>
+          </div>
+   
